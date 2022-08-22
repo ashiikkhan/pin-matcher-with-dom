1672e03e3adb860ebdd0cb7d6d57164a9661ba65
ashiikkhan <ashiksust06@gmail.com>
2022-08-22 13:53:20 +0600

    pin matcher complete

--- js/app.js
+++ js/app.js
@@ -17,3 +17,47 @@ pinGenerateBtn.addEventListener('click', function () {
   const displayEl = document.getElementById('display-pin');
   displayEl.value = pin;
 });
+// deligattion:
+const calculator = document.getElementById('calculator');
+calculator.addEventListener('click', function (event) {
+  const number = event.target.innerText;
+  const typedNumberField = document.getElementById('number-field');
+  const previousTypedNumber = typedNumberField.value;
+  if (isNaN(number)) {
+    if (number === 'C') {
+      typedNumberField.value = '';
+    } else if (number === '<') {
+      const digits = previousTypedNumber.split('');
+      digits.pop();
+      const remainningDigits = digits.join('');
+      typedNumberField.value = remainningDigits;
+    }
+  } else {
+    const newTypedNumber = previousTypedNumber + number;
+    typedNumberField.value = newTypedNumber;
+  }
+});
+// pin match
+
+const verifyBtn = document.getElementById('verify-pin');
+verifyBtn.addEventListener('click', function () {
+  const generatedPin = document.getElementById('display-pin').value;
+  const typedPin = document.getElementById('number-field').value;
+
+  const incorrectPinMessage = document.getElementById('incorrect-pin');
+  const correctPinMessage = document.getElementById('correct-pin');
+  if (generatedPin === typedPin) {
+    correctPinMessage.style.display = 'block';
+    incorrectPinMessage.style.display = 'none';
+  } else {
+    incorrectPinMessage.style.display = 'block';
+    correctPinMessage.style.display = 'none';
+  }
+});
+// refresh button :
+document.getElementById('refresh-btn').addEventListener('click', function () {
+  document.getElementById('display-pin').value = '';
+  document.getElementById('number-field').value = '';
+  document.getElementById('correct-pin').style.display = 'none';
+  document.getElementById('incorrect-pin').style.display = 'none';
+});
